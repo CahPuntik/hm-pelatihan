256ac470ff9bd050e28e64b3b6339ec40058f3f0
Slamet Herianto <heri.fromrfive@gmail.com>
2026-01-18 21:59:30 +0800

    Improve PWA: Better icons, manifest, service worker, and install detection

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
-const CACHE_NAME = "hm-pelatihan-v3";
+const CACHE_NAME = "hm-pelatihan-v4";
 const urlsToCache = [
   "/hm-pelatihan/",
   "/hm-pelatihan/index.html",
   "/hm-pelatihan/manifest.json",
+  "/hm-pelatihan/icon-192.svg",
+  "/hm-pelatihan/icon-512.svg"
 ];
+
+console.log('Service Worker loaded');
 
 // Install event
 self.addEventListener("install", (event) => {
+  console.log('Service Worker installing...');
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting()),
+      .then((cache) => {
+        console.log('Caching app files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log('✅ Cache ready');
+        self.skipWaiting();
+      })
+      .catch(err => console.error('Install failed:', err))
   );
 });
 
